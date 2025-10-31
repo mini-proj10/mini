ec7e93165aedea93882adc1130f49f0194ab377e
ymle <babdoy123@gmail.com>
2025-10-31 12:47:28 +0900

    Merge branch 'main' of https://github.com/mini-proj10/mini

--- frontend/src/utils/kakaoStaticMap.jsx
+++
@@ -1,5 +0,0 @@
-// src/utils/kakaoStaticMap.js
-export function getKakaoStaticMapUrl({ lat, lng, level = 4 }) {
-    return `http://localhost:8000/api/map-capture?lat=${lat}&lng=${lng}&level=${level}`;
-  }
-  
